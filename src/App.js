993ccbb9374b9c6f9c4030bb6fcf07a1eb2be7d0
CoderShobhitSingh <snghshobhit@gmail.com>
2025-04-20 21:34:27 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [factorial, setFactorial] = useState(0);
+
+  const numFact = () => {
+    const num = parseInt(number); // to convert the string to number
+
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result = result * i;
+    }
+
+    setFactorial(result);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          placeholder="Enter A number"
+        />
+        <button onClick={numFact}>Calculate</button>
+        <p>{`factorial of ${number} is : ${factorial}`}</p>
+      </div>
+    </>
   );
 }
 
